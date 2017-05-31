f4139fa44c6037df1a84f9d2509dade1dae32810
idlviv <idlviv@rambler.ru>
2017-05-31 18:20:33 +0300

    register frontend DONE

--- src/src/app/components/register/register.component.ts
+++ src/src/app/components/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service';
+import {AuthService} from '../../services/auth.service';
 import {FlashMessagesService} from 'angular2-flash-messages';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -15,7 +16,11 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private validateService: ValidateService,
-    private flashMessage: FlashMessagesService) {}
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService,
+
+  ) {}
 
   ngOnInit() {
   }
@@ -26,7 +31,7 @@ export class RegisterComponent implements OnInit {
       email: this.email,
       username: this.username,
       password: this.password
-    }
+    };
     if (!this.validateService.validateRegister(user)) {
       this.flashMessage.show(
         'fill all fields',
@@ -46,7 +51,26 @@ export class RegisterComponent implements OnInit {
         });
       return false;
     }
-    console.log('ok');
+  this.authService.registerUser(user)
+    .subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show(
+          'Registration successful',
+          {
+            cssClass: 'alert-success',
+            timeout: 3000
+          });
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show(
+          'Registration failed',
+          {
+            cssClass: 'alert-danger',
+            timeout: 3000
+          });
+        this.router.navigate(['/register']);
+      }
+    })
   }
 
 }
