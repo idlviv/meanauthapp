51c42a71a8a44b442e5b56aaa615df0fb307e97e
idlviv <idlviv@rambler.ru>
2017-06-02 07:46:35 +0300

    +

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/dist'));
 });
 
 app.use(function(err, req, res, next) {
